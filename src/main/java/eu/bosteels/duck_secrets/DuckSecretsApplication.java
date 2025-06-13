c321d6dd7a2d93a999688b5829ed66897a633002
Maarten Bosteels <maarten.bosteels@dnsbelgium.be>
2025-06-13 16:43:59 +0200

    initial

---
+++ src/main/java/eu/bosteels/duck_secrets/DuckSecretsApplication.java
@@ -0,0 +1,13 @@
+package eu.bosteels.duck_secrets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckSecretsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DuckSecretsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/eu/bosteels/duck_secrets/DuckSecretsApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.bosteels.duck_secrets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckSecretsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
